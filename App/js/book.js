24e17ea3a030077a444a3d1890a53b905e9b1572
SamShiels <sam.shiels@derivco.co.za>
2018-07-17 12:30:55 +0200

    Changed module to 'UMD'

--- App/js/book.js
+++ App/js/book.js
@@ -1,24 +1,34 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Book = /** @class */ (function () {
-    function Book(newTitle, newCategory) {
-        this.title = newTitle;
-        this.category = newCategory;
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
     }
-    Object.defineProperty(Book.prototype, "title", {
-        get: function () {
-            return this._title.toUpperCase();
-        },
-        set: function (val) {
-            this._title = val;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Book.prototype.hello = function (thing) {
-        return thing;
-    };
-    return Book;
-}());
-exports.Book = Book;
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Book = /** @class */ (function () {
+        function Book(newTitle, newCategory) {
+            this.title = newTitle;
+            this.category = newCategory;
+        }
+        Object.defineProperty(Book.prototype, "title", {
+            get: function () {
+                return this._title.toUpperCase();
+            },
+            set: function (val) {
+                this._title = val;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Book.prototype.hello = function (thing) {
+            return thing;
+        };
+        return Book;
+    }());
+    exports.Book = Book;
+});
 //# sourceMappingURL=book.js.map
--- App/js/util.js
+++ App/js/util.js
@@ -1,13 +1,23 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Util = /** @class */ (function () {
-    function Util() {
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
     }
-    Util.prototype.Sqr = function (input) {
-        // Squares the input value and returns it
-        return input * input;
-    };
-    return Util;
-}());
-exports.Util = Util;
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Util = /** @class */ (function () {
+        function Util() {
+        }
+        Util.prototype.Sqr = function (input) {
+            // Squares the input value and returns it
+            return input * input;
+        };
+        return Util;
+    }());
+    exports.Util = Util;
+});
 //# sourceMappingURL=util.js.map
